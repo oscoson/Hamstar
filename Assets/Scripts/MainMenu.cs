701a0a5eb7ce0991f4516b65fee94b7f67651d62
OscoSon <oscarsani007@gmail.com>
2022-06-04 01:25:31 -0400

    Main Menu Improvements

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject optionsCanvas;
+
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+
+}
