2cff12b1710b74d73231c3aca77f3b03a72e5920
Brian Cho <b.cho@queensu.ca>
2022-06-03 21:16:47 -0400

    Update NormalPlanet.cs
    adjust G value

--- Assets/Scripts/NormalPlanet.cs
+++ Assets/Scripts/NormalPlanet.cs
@@ -12,7 +12,7 @@ public class NormalPlanet : Planet
 
     public override Vector2 GetPullForce(Rigidbody2D rb)
     {
-        const float G = 1;
+        const float G = 100;
         Vector2 rawDirection = planetRb.position - rb.position;
         Vector2 normDirection = rawDirection.normalized;
         float lengthSqr = rawDirection.SqrMagnitude();
