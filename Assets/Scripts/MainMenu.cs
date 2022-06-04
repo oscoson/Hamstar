202afb00b5b52538909479d9a7fe8b384e0feea1
OscoSon <oscarsani007@gmail.com>
2022-06-04 01:42:42 -0400

    Main Menu final changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 
-    public GameObject optionsCanvas;
-
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
 
     public void QuitButton()
     {
