01fb5d905cae8a4d0ac35fe3140d6b5e13ea02ec
OscoSon <oscarsani007@gmail.com>
2022-06-03 20:50:07 -0400

    Circle launched functionality added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D playerRB;
+    [SerializeField] private float launchSpeed = 5f;
+
+    [SerializeField] private bool launched = false;
 
 
     // Start is called before the first frame update
@@ -15,6 +19,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1") && !(launched))
+        {
+            Launch();
+        }
+    }
+
+    public void Launch()
+    {
+        launched = true;
+        Rigidbody2D rb = playerRB.GetComponent<Rigidbody2D>();
+        rb.AddForce(-(launchSpeed) * playerRB.position, ForceMode2D.Impulse);
     }
 }
