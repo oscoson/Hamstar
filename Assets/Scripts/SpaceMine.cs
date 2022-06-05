73fcaae59c164a8124f39b387dd5b261950e7d7f
Brian Cho <b.cho@queensu.ca>
2022-06-04 20:38:51 -0400

    SpaceMine Sprites

--- Assets/Scripts/SpaceMine.cs
+++ Assets/Scripts/SpaceMine.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class SpaceMine : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    private float blinkTimer;
+    private const float blinkTime = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        blinkTimer = blinkTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        blinkTimer -= Time.deltaTime;
+        if (blinkTimer < 0.2f)
+        {
+            spriteRenderer.color = new Color(1, 0.25f, 0.25f, 1);
+            if(blinkTimer < 0.0f)
+            {
+                blinkTimer = blinkTime;
+                spriteRenderer.color = Color.white;
+
+            }
+        } else
+        {
+            spriteRenderer.color = Color.white;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
